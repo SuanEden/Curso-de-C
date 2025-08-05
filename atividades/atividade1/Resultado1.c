6a905114400c4a315a57d8f6682cc0e45fd04e8f
SuanEden <silveirasuan@gmail.com>
2025-08-05 15:57:25 -0300

    Cria  Resultado 1:3

---
+++ atividades/atividade1/Resultado1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+   printf("Olá Eden, estou começando a programar em C!");
+    return 0;
+}
+
+
---
+++ atividades/atividade2/Resultado2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    char nome[30];
+    int idade;
+    float altura;
+
+    printf("Qual o seu nome: ");
+    scanf("%s", nome);
+    printf("Qual a sua idade: ");
+    scanf("%d", &idade);
+    printf("Qual a sua altura: ");
+    scanf("%f", &altura);
+
+    printf("\nSeu nome é: %s\nSua idade é: %d\nSua altura é %.2f\n",nome,idade, altura);
+    return 0;
+}
