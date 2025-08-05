8ef9fb18405e46da114280de445033330fe3a557
SuanEden <silveirasuan@gmail.com>
2025-08-05 16:58:05 -0300

    Atualiza  Resultado3.c | Cria Resultado4.c

---
+++ atividades/atividade4/Resultado4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    float peso ,altura;
+    printf("Me informe a sua altura: ");
+        scanf("%f", &altura);
+    printf("\nMe infome a seu peso: ");
+        scanf("%f", &peso);
+    
+    printf("\nO seu IMC é: %.2f", peso/(altura*altura));
+    return 0;
+}
