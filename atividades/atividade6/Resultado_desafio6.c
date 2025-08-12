863daea747dd4aaf166ebfc446b1bd088a00acbd
SuanEden <silveirasuan@gmail.com>
2025-08-12 19:47:44 -0300

    Atualiza Resultado_desafio6.c

---
+++ atividades/atividade6/Resultado_desafio6.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int numero, tabuada, operadores;
+        printf("Me diga um valor: ");
+            scanf("%d", &numero);
+        printf("Qual a operador que você quer usar? ");
+        printf("\n1 - Adicao\n2 - Substracao\n3 - Mutiplicacao\n4 - Divisao\n");
+            scanf("%d", &operadores);
+
+        switch (operadores)
+        {
+        case 1:
+            for ( tabuada = 0; tabuada <= 10; tabuada++)
+            {
+             printf("\n%d + %d = %d", numero, tabuada, numero+tabuada);
+            }
+            break;
+        case 2:
+            for ( tabuada = 0; tabuada <= 10; tabuada++)
+            {
+           printf("\n%d - %d = %d", numero, tabuada, numero-tabuada);
+            }
+            break;
+        case 3:
+            for ( tabuada = 0; tabuada <= 10; tabuada++)
+            {
+           printf("\n%d * %d = %d", numero, tabuada, numero*tabuada);
+            }
+            break; 
+        case 4:
+            for ( tabuada = 0; tabuada <= 10; tabuada++)
+            {
+           printf("\n%d / %d = %.2f", numero, tabuada, (float)numero / tabuada);
+            }
+            break;    
+        default:
+        printf("Numero invalido");
+            break;
+        }
+
+return 0;
+}
