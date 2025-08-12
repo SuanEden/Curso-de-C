142f70f577fcf9eb85c1a8be145a93fe1e970134
SuanEden <silveirasuan@gmail.com>
2025-08-12 20:24:14 -0300

    Atualiza Resultado_Desafio1.c

--- desafio_intermediarios/Resultado_Desafio1.c
+++ desafio_intermediarios/Resultado_Desafio1.c
@@ -2,7 +2,26 @@
 
  int main(int argc, char const *argv[])
  {
-    
+    int palpite, numero = 10;
+        printf("De o seu primeiro palpite: ");
+    while (palpite != numero)
+    {
+        scanf("%d", &palpite);
+        if (palpite>numero)
+        {
+            printf("\nMais pra baixo ");
+        }else if (palpite<numero)
+        {
+            printf("\nMais para cima ");
+        }
+        if (palpite != numero)
+        {        
+            printf("\nTente de novo: ");
+        }
+        
+        
+    }
+    printf("\nParabens, voce acertou o numero %d", numero);
     return 0;
  }
  
