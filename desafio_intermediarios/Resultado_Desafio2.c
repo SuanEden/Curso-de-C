4ec796ffdb2e3a4a2fa6d9504094b68d680199a4
SuanEden <silveirasuan@gmail.com>
2025-08-13 00:30:36 -0300

    Atualiza Resultado_Desafio2.c

---
+++ desafio_intermediarios/Resultado_Desafio2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+float obterPeso (){
+    float peso;
+        printf("Qual o seu peso? ");
+        scanf("%f", &peso);
+        return peso;
+}
+
+float obterAltura(){
+    float altura;
+        printf("Qual a sua altura?");
+        scanf("%f", &altura);
+    return altura;
+}
+float calcularIMC(float peso, float altura){
+    return peso/(altura*altura);
+}
+
+ int main(int argc, char const *argv[])
+ {
+    float peso = obterPeso();
+    float altura = obterAltura();
+    float imc = calcularIMC(peso,altura);
+
+    printf("%.2f %.2f %.2f",peso,altura,imc);
+    return 0;
+ }
+ 
