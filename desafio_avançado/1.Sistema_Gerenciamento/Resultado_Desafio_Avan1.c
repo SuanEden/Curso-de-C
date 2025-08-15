22d9dcf78fcfac341802ced7c4e946b629ac5ea3
SuanEden <silveirasuan@gmail.com>
2025-08-15 16:28:46 -0300

    Cria Função coletaNotas

---
+++ desafio_avançado/1.Sistema_Gerenciamento/Resultado_Desafio_Avan1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#define ALUNOS 3
+#define PROVAS 4
+
+void coletaNotas(float notas[ALUNOS][PROVAS])
+{
+    int i, j, n = 1;
+    for (i = 0; i < ALUNOS; i++)
+    {
+            printf("\nQuais as notas do aluno %d\n",i+1);
+        for (j = 0; j < PROVAS; j++)
+        {
+            printf("Prova %d: ", n);
+          scanf("%f", &notas[i][j]);
+          n++;
+        }
+          printf("\n");
+        n = 1;
+    }
+    return notas[ALUNOS][PROVAS];
+}
+
+int main(int argc, char const *argv[])
+{
+    float notas[ALUNOS][PROVAS];
+    coletaNotas(notas);
+    int i , j, n;
+    for (i = 0; i < ALUNOS; i++)
+    {
+       printf("\nAluno : %d \n", i+1);
+        for (j = 0; j < PROVAS; j++)
+        {
+         printf("Nota da prova %d: %f\n",n, notas[i][j]);
+         n++;
+        }
+        printf("\n");
+        n=0;
+    }
+    
+    
+    return 0;
+}
+
