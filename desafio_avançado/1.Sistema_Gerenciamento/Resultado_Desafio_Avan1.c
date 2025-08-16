03ee926cff2e12425a101b4020c3f3b10056b685
SuanEden <silveirasuan@gmail.com>
2025-08-15 21:39:02 -0300

    Cria Função calcularMediaAluno

--- desafio_avançado/1.Sistema_Gerenciamento/Resultado_Desafio_Avan1.c
+++ desafio_avançado/1.Sistema_Gerenciamento/Resultado_Desafio_Avan1.c
@@ -2,7 +2,7 @@
 #define ALUNOS 3
 #define PROVAS 4
 
-void coletaNotas(float notas[ALUNOS][PROVAS])
+      void coletaNotas(float notas[ALUNOS][PROVAS])
 {
     int i, j, n = 1;
     for (i = 0; i < ALUNOS; i++)
@@ -17,28 +17,76 @@ void coletaNotas(float notas[ALUNOS][PROVAS])
           printf("\n");
         n = 1;
     }
-    return notas[ALUNOS][PROVAS];
 }
-void imprimiNotas(float notas[ALUNOS][PROVAS]){
-int i , j, n;
+      float calcularMediaAluno(float notas[ALUNOS][PROVAS], float media[ALUNOS]){
+          int  i, j;
+          float media_t = 0.0f;
+          
+          for (i = 0; i < ALUNOS; i++)
+          {
+            media[i] = 0.0f;
+            
+            for ( j = 0; j < PROVAS; j++)
+            {
+              media[i] += notas[i][j];
+              
+            }
+            media[i] = media[i] / PROVAS;
+              
+              media_t+=media[i];
+
+            }
+            media_t /= ALUNOS;
+         
+              printf("\n \n");
+
+          return media_t;
+      }
+
+      void imprimiNotas(float notas[ALUNOS][PROVAS], float media[ALUNOS]){
+      int i , j, n;
+      float media_t = calcularMediaAluno(notas,media);
     for (i = 0; i < ALUNOS; i++)
     {
+    
+      n = 0;
        printf("\nAluno : %d \n", i+1);
+       
         for (j = 0; j < PROVAS; j++)
         {
          printf("Nota da prova %d: %f\n",n, notas[i][j]);
          n++;
+          
         }
-        printf("\n");
-        n=0;
+              printf("\nMedia do Aluno %d: %f \n",i+1 ,media[i]);
+        
+    
     }
+        printf("\n\n");
+        for (i = 0; i < ALUNOS; i++)
+    {
+      printf("Media do Aluno %d: %f \n",i+1 ,media[i]);
+    }
+    printf("\nMedia da sala e: %f", media_t);
 
-}
+    }
+    
+
+
+
+    
+
+
 int main(int argc, char const *argv[])
 {
+  
+    float media[ALUNOS+1];
     float notas[ALUNOS][PROVAS];
     coletaNotas(notas);
-    imprimiNotas(notas);
+    calcularMediaAluno(notas, media);
+    imprimiNotas(notas, media);
+    
+    
    return 0;
 }
 
