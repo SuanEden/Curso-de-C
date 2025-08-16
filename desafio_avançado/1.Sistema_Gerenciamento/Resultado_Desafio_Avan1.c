2bc1eb763ad049174527427a49d891a76fca61c8
SuanEden <silveirasuan@gmail.com>
2025-08-15 22:19:32 -0300

    Atualiza Resultado_Desafio_Avan1.c

--- desafio_avançado/1.Sistema_Gerenciamento/Resultado_Desafio_Avan1.c
+++ desafio_avançado/1.Sistema_Gerenciamento/Resultado_Desafio_Avan1.c
@@ -2,91 +2,59 @@
 #define ALUNOS 3
 #define PROVAS 4
 
-      void preencherNotas(float notas[ALUNOS][PROVAS])
-{
-    int i, j, n = 1;
-    for (i = 0; i < ALUNOS; i++)
-    {
-            printf("\nQuais as notas do aluno %d\n",i+1);
-        for (j = 0; j < PROVAS; j++)
-        {
-            printf("Prova %d: ", n);
-          scanf("%f", &notas[i][j]);
-          n++;
-        }
-          printf("\n");
-        n = 1;
+void preencherNotas(float notas[ALUNOS][PROVAS], int i, int j){
+  for (i = 0; i < ALUNOS; i++){
+    printf("\nQuais as notas do aluno %d\n",i+1);
+    for (j = 0; j < PROVAS; j++){
+      printf("Prova %d: ", j+1);
+      scanf("%f", &notas[i][j]);
+    }
+    printf("\n");
     }
 }
-      float calcularMediaAluno(float notas[ALUNOS][PROVAS], float media[ALUNOS]){
-          int  i, j;
-          float media_t = 0.0f;
-          
-          for (i = 0; i < ALUNOS; i++)
-          {
-            media[i] = 0.0f;
-            
-            for ( j = 0; j < PROVAS; j++)
-            {
-              media[i] += notas[i][j];
-              
-            }
-            media[i] = media[i] / PROVAS;
-              
-              media_t+=media[i];
 
-            }
-            media_t /= ALUNOS;
-         
-              printf("\n \n");
+float calcularMediaAluno(float notas[ALUNOS][PROVAS], float media[ALUNOS], int i, int j){
+  float media_t = 0.0f;
 
-          return media_t;
-      }
-
-      void exibirResultados(float notas[ALUNOS][PROVAS], float media[ALUNOS]){
-      int i , j, n;
-      float media_t = calcularMediaAluno(notas,media);
-    for (i = 0; i < ALUNOS; i++)
-    {
-    
-      n = 0;
-       printf("\nAluno : %d \n", i+1);
-       
-        for (j = 0; j < PROVAS; j++)
-        {
-         printf("Nota da prova %d: %f\n",n, notas[i][j]);
-         n++;
-          
-        }
-              printf("\nMedia do Aluno %d: %f \n",i+1 ,media[i]);
-        
-    
+  for (i = 0; i < ALUNOS; i++){
+    media[i] = 0.0f;
+    for (j = 0; j < PROVAS; j++){
+      media[i] += notas[i][j];
     }
-        printf("\n\n");
-        for (i = 0; i < ALUNOS; i++)
-    {
-      printf("Media do Aluno %d: %f \n",i+1 ,media[i]);
+    media[i] /= PROVAS;
+    media_t+=media[i];
+  }
+  media_t /= ALUNOS;
+  printf("\n \n");
+
+  return media_t;
+}
+
+void exibirResultados(float notas[ALUNOS][PROVAS], float media[ALUNOS], float media_t, int i, int j){
+  for (i = 0; i < ALUNOS; i++){
+    printf("\nAluno : %d \n", i+1);
+    for (j = 0; j < PROVAS; j++){
+      printf("Nota da prova %d: %f\n",j+1, notas[i][j]);
     }
-    printf("\nMedia da sala e: %f", media_t);
-
-    }
-    
-
-
-
-    
-
-
+  printf("\nMedia do Aluno %d: %f \n",i+1 ,media[i]);
+  }
+  printf("\n\n");
+  for (i = 0; i < ALUNOS; i++){
+    printf("Media do Aluno %d: %f \n",i+1 ,media[i]);
+  }
+  printf("\nMedia da sala e: %f", media_t);
+}
+  
 int main(int argc, char const *argv[])
 {
-  
-    float media[ALUNOS+1];
-    float notas[ALUNOS][PROVAS];
-    preencherNotas(notas);
-    calcularMediaAluno(notas, media);
-    exibirResultados(notas, media);
-    
-    
-   return 0;
+  int i, j;
+  float media[ALUNOS];
+  float notas[ALUNOS][PROVAS];
+    preencherNotas(notas, i, j);
+    calcularMediaAluno(notas, media, i, j);
+    float media_t = calcularMediaAluno(notas, media, i, j );
+    exibirResultados(notas, media, media_t, i, j);
+
+  return 0;
 }
 
