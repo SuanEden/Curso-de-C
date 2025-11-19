6e156a739610b37677064c98b93bed48bb5e0e3a
SuanEden <silveirasuan@gmail.com>
2025-11-19 10:45:58 -0300

    Ajuste nome de varivel Atv.6

--- atividades/6.Tabuada_Laço/Eden.c
+++ atividades/6.Tabuada_Laço/Eden.c
@@ -3,22 +3,22 @@
 int main(int argc, char const *argv[])
 {
     int Num,Operado;
-    float Tabuda;
+    float Tabuada;
     printf("Me diga um numero: ");
     scanf("%d", &Num);
     printf(" \n1 - Soma \n2 - Subtração \n3 - Multiplicação \n4 - Divisão \nEscolha o operador:");
     scanf("%d",&Operado);
-    for(Tabuda = 0; Tabuda <=10;Tabuda++){
+    for(Tabuada = 0; Tabuada <=10;Tabuada++){
         switch (Operado)
         {
         case 1:
-        printf("\n%d + %2.f = %2.f",Num,Tabuda, Num+Tabuda);
+        printf("\n%d + %2.f = %2.f",Num,Tabuada, Num+Tabuada);
         case 2:
-        printf("\n%d - %2.f = %2.f",Num,Tabuda, Num-Tabuda);
+        printf("\n%d - %2.f = %2.f",Num,Tabuada, Num-Tabuada);
         case 3:
-        printf("\n%d * %2.f = %2.f",Num,Tabuda, Num*Tabuda);
+        printf("\n%d * %2.f = %2.f",Num,Tabuada, Num*Tabuada);
         case 4:
-        printf("\n%d / %2.f = %2.f",Tabuda,Num, Num/Tabuda);
+        printf("\n%d / %2.f = %2.f",Tabuada,Num, Num/Tabuada);
         }
     }
     
